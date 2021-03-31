ca64a58f12e4fff6e1bf0388c7b326a2c86c7a02
bahodirov <daniyarovich711@gmail.com>
2021-04-01 00:01:31 +0500

    bu mening ikkinchi commitim

--- task1.cpp
+++ task1.cpp
@@ -1,11 +1,53 @@
 #include <iostream>
 using namespace std;
 
+
+
 int main() {
-	int a, b, c;
-	
-	cin >> a >> b >> c;
-	cout << a + b + c;
+	int a;
+	bool juft;
+	bool toq;
+
+	cin >> a;
+
+	juft = a % 2 == 0;
+	toq = a % 2 == 1;
+
+	if (juft) {
+		
+		if (a < 10) {
+			cout << "bu juft son va 10dan kichkina" << endl;
+		}
+
+		else {
+			cout << "bu juft son" << endl;
+		}
+	}
+
+	else if (toq) {
+		cout << "bu toq son" << endl;
+	}
+
+	else {
+		cout << "xatolik yuz berdi";
+	}
+
+	// if (a % 2 == 0) {
+	// 	cout << "bu juft son" << endl;
+	// }
+
+	// else if (a % 2 == 1) {
+	// 	cout << "bu toq son" << endl;
+	// }
+
+	// if (a < 5) {
+	// 	cout << "birinchisi: " << a << endl;
+	// }
+
+	// else if (a < 10) {
+	// 	cout << "ikkinchisi: " << a << endl;
+	// }
+
 	
 	return 0;
-} 
+}
